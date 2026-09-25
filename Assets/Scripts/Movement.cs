fc481cbeac5a30031dac534108a0ce0bcef7eb2d
JFitzyz <j.fitzsimons0724@gmail.com>
2026-09-25 16:11:04 -0400

    CharacterMovement
    Character movement added to player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,17 +3,20 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
+    // These variables are to hold the Action references
     InputAction moveAction;
 
     private void Start()
     {
+        // Find the references to the "Move" and "Jump" actions
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
     void Update()
     {
+        // Read the "Move" action value, which is a 2D vector
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        
-        transform.Translate(new Vector3((float)(moveValue.x * 0.1), moveValue.y, 0));
+        // your movement code here
+        transform.Translate(new Vector3(moveValue.x, moveValue.y, 0));
     }
 }
